354f8eb7e2b7a6437c79bde549c66754765b0869
jum648 <78852579+jum648@users.noreply.github.com>
2021-03-03 15:20:48 +0300

    Add files via upload

---
+++ juma.py
@@ -0,0 +1,22 @@
+car ={
+"brand" : "Ford",
+"model":"mustang",
+"year" : 1964
+}
+if "model" in car:
+  print( "yes 'model' is  one of the keys in this dict dictionary")
+
+car ={
+ "brand" : "Ford",
+"model": "mustang",
+"year" :1964
+}
+car["year"]=2018
+print (car)
+
+car={
+ "brand" :"ford",
+ "model" :"mustang",
+ "year" :1964
+}
+car["color"] =("red","blue","red")
